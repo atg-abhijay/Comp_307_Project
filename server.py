b7ed10d085d62446729645037e39ef57fc76a68b
atg-abhijay <atg.abhijay@gmail.com>
2017-12-04 22:28:56 -0500

    removed example code

--- server.py
+++ server.py
@@ -295,10 +295,3 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 
-
-# @app.route('/articles_list/contents/')
-# def json_contents():
-#     response = make_response(jsonify(response=get_articles(ARTICLES_NAME)))    response.headers['Access-Control-Allow-Origin'] = '*'
-#     response.headers['Access-Control-Allow-Methods'] = 'POST'
-#     response.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
-#     return response
